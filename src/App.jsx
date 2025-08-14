4a2346a97878327dbfc48a139ceade01a1f770d8
Suprio-Das <priyamd507@gmail.com>
2025-08-15 00:29:49 +0600

    daisyui default table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import './App.css'
+import { addTask } from './Redux/TaskSlice';
 
 function App() {
   const dispatch = useDispatch();
@@ -11,7 +12,8 @@ function App() {
     const deadline = form.deadline.value;
     const newTask = { name, details, deadline };
     if (name === '' || details === '' || deadline === '') return;
-    console.log(newTask)
+    dispatch(addTask(newTask))
+    form.reset();
   }
   return (
     <div>
@@ -26,6 +28,45 @@ function App() {
             <button className='btn bg-green-500 w-xs text-black'>Add New Task</button>
           </div>
         </form>
+        <h2 className='text-2xl text-center font-semibold my-5'>Current TaskList</h2>
+        <section className='w-3/4 mx-auto'>
+          <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Favorite Color</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>1</th>
+                  <td>Cy Ganderton</td>
+                  <td>Quality Control Specialist</td>
+                  <td>Blue</td>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <th>2</th>
+                  <td>Hart Hagerty</td>
+                  <td>Desktop Support Technician</td>
+                  <td>Purple</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <th>3</th>
+                  <td>Brice Swyre</td>
+                  <td>Tax Accountant</td>
+                  <td>Red</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
       </section>
     </div>
   )
