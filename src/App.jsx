f3845869a3ebc41aba279e4d311bcc58aa4133f4
Suprio-Das <priyamd507@gmail.com>
2025-08-14 22:51:58 +0600

    Headings defined

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <h1 className='text-3xl font-semibold text-center my-3'>Task Manager</h1>
+      <section>
+        <h2 className='text-xl text-center'>Add New Task</h2>
+      </section>
+    </div>
   )
 }
 
