4cb96c1f2c7627b2b62fe02b04fa19954cc7aea4
Suprio-Das <priyamd507@gmail.com>
2025-08-15 00:41:03 +0600

    Task displayed with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
 import { addTask } from './Redux/TaskSlice';
 
 function App() {
+  const tasks = useSelector((state) => state.task)
   const dispatch = useDispatch();
   const handleNewTaskSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +16,7 @@ function App() {
     dispatch(addTask(newTask))
     form.reset();
   }
+  console.log(tasks)
   return (
     <div>
       <h1 className='text-3xl font-semibold text-center my-3'>Task Manager</h1>
@@ -30,47 +32,37 @@ function App() {
         </form>
         <h2 className='text-2xl text-center font-semibold my-5'>Current TaskList</h2>
         <section className='w-3/4 mx-auto'>
-          <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Details</th>
-                  <th>Deadline</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>
-                    <button className='btn btn-success btn-xs me-2'>Mark Done</button>
-                    <button className='btn btn-error btn-xs'>Delete</button>
-                  </td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                  <th>2</th>
-                  <td>Hart Hagerty</td>
-                  <td>Desktop Support Technician</td>
-                  <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                  <th>3</th>
-                  <td>Brice Swyre</td>
-                  <td>Tax Accountant</td>
-                  <td>Red</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          {
+            tasks.length > 0 ?
+              <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                <table className="table">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>Details</th>
+                      <th>Deadline</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      tasks.map((task, index) => <tr key={index}>
+                        <th>{index + 1}</th>
+                        <td>{task.name}</td>
+                        <td>{task.details}</td>
+                        <td>{task.deadline}</td>
+                        <td>
+                          <button className='btn btn-success btn-xs me-2'>Mark Done</button>
+                          <button className='btn btn-error btn-xs'>Delete</button>
+                        </td>
+                      </tr>)
+                    }
+                  </tbody>
+                </table>
+              </div> : "No TASKS YET!!!"
+          }
         </section>
       </section>
     </div>
