e2ce4846433dcec13f2a087615e165cca2e6a57c
Suprio-Das <priyamd507@gmail.com>
2025-08-14 23:35:02 +0600

    new task adding field and button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ function App() {
       <h1 className='text-3xl font-semibold text-center my-3'>Task Manager</h1>
       <section>
         <h2 className='text-xl text-center'>Add New Task</h2>
+        <form action="">
+          <div className='flex flex-col items-center justify-center gap-5 my-3'>
+            <input type="text" placeholder="Enter Task Name" className="input" name='taskName' />
+            <input type="text" placeholder="Enter Task Details" className="textarea" name='taskDetails' />
+            <input type="text" placeholder="Enter Deadline" className="input" name='deadline' />
+            <button className='btn bg-green-500 w-xs text-black'>Add New Task</button>
+          </div>
+        </form>
       </section>
     </div>
   )
